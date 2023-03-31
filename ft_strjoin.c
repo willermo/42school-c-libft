5cffbc73b192d226210a32bba93c344d4af22e37
davide.oriani <willermo@gmail.com>
2023-03-31 18:11:24 +0200

    Fixed prohibited references on ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: doriani <doriani@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 14:16:24 by doriani           #+#    #+#             */
-/*   Updated: 2023/03/30 02:57:28 by doriani          ###   ########.fr       */
+/*   Updated: 2023/03/31 18:10:28 by doriani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s1, strlen(s1) + 1);
-	ft_strlcat(str, s2, strlen(s1) + strlen(s2) + 1);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
 	return (str);
 }
