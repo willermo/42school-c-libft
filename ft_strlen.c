ea2c1f841f94b383bce0cc73e4c425bfb67e197e
davide.oriani <willermo@gmail.com>
2023-04-02 22:29:32 +0200

    Minor corrections

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: doriani <doriani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: doriani <doriani@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:12:36 by doriani           #+#    #+#             */
-/*   Updated: 2023/03/29 09:07:35 by doriani          ###   ########.fr       */
+/*   Updated: 2023/04/02 22:27:05 by doriani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 size_t	ft_strlen(const char *str)
 {
-	int	len;
+	size_t	len;
 
+	if (!str)
+		return (0);
 	len = 0;
 	while (*str++)
 		len++;
